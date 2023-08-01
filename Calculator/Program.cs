8d96ba288b7407038860d355ee88d79aef7ea5ac
Wiktor <wiktor.tatarynowicz@gmail.com>
2023-08-01 09:13:59 +0200

    calculator structure

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -23,8 +23,35 @@
                     Console.WriteLine("2. Subtraction");
                     Console.WriteLine("3. Multiplication");
                     Console.WriteLine("4. Divide");
+                    Console.WriteLine("5. Out");
                     Console.WriteLine();
                     Console.Write("Your chose: ");
+                    int chose2=int.Parse(Console.ReadLine());
+
+                    if( chose2 == 1)
+                    {
+
+                    }
+                    if(chose2 == 2)
+                    {
+
+                    }
+                    if (chose2 == 3)
+                    {
+
+                    }
+                    if (chose2 == 4)
+                    {
+
+                    }
+                    if(chose2 == 5)
+                    {
+                        run = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong number");
+                    }
                 }
                 if(chose == 2)
                 {
